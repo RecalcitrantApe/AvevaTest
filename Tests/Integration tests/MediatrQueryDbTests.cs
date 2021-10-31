620beef49015c5d06cccb6c7c55532895901aa84
Claus Lisberg <clauswlisberg@gmail.com>
2021-10-31 19:18:44 +0100

    Unit tests

---
+++ Tests/Integration tests/MediatrQueryDbTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using EmployeeApi.Application.Common.Interfaces;
+using EmployeeApi.Application.Employees.Queries;
+using EmployeeApi.Application.Mappings;
+using EmployeeApi.Domain.Entities;
+using EmployeeApi.Infrastructure.Persistence;
+using EmployeeApi.Web.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Tests.Integration_tests
+{
+    public class MediatrQueryDbTests
+    {
+        private readonly Mock<ILogger<GetEmployeeQuery>> logger;
+        private readonly Mock<IMediator> mockMediator;
+        private readonly IConfigurationProvider configuration;
+        private readonly IMapper mapper;
+
+
+        public MediatrQueryDbTests()
+        {
+            this.logger = new Mock<ILogger<GetEmployeeQuery>>();
+            this.mockMediator = new Mock<IMediator>();
+
+            this.configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            this.mapper = configuration.CreateMapper();
+        }
+
+        [Test]
+        public async Task GetEmployeeShouldReturnEmployee()
+        {
+
+            //Arange
+            ICurrentUserService currentUserService = new DummyCurrentUserService();
+
+            var options = new DbContextOptionsBuilder<EmployeeDbContext>()
+               .UseInMemoryDatabase(databaseName: "TestDatabase")
+               .Options;
+            using (var context = new EmployeeDbContext(options, currentUserService))
+            {
+                context.Employees.Add(new Employee { FirstName = "Test", LastName = "Testessen", DateOfBirth = EmployeeApi.Domain.ValueObjects.DateOnly.From(DateTime.Now), Email = EmployeeApi.Domain.ValueObjects.Email.From("test@test.com"), });
+                context.SaveChanges();
+
+                GetEmployeeQuery command = new GetEmployeeQuery(1);
+                GetEmployeeQueryHandler handler = new GetEmployeeQueryHandler(context, this.mapper);
+
+                //Act through mediator
+                EmployeeDto employee = await handler.Handle(command, new System.Threading.CancellationToken());
+
+                employee.Should().NotBeNull();
+                
+            }
+        }
+
+    }
+}
---
+++ Tests/Unit tests/Application/MappingTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using EmployeeApi.Application.Employees.Queries;
+using EmployeeApi.Application.Mappings;
+using EmployeeApi.Domain.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Unit_tests.Application
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Test]
+        public void MustHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        [TestCase(typeof(Employee), typeof(EmployeeDto))]
+        public void ShouldMapEmployee(Type source, Type destination)
+        {
+            var instance = GetInstanceOf(source);
+            _mapper.Map(instance, source, destination);
+        }
+
+        private object GetInstanceOf(Type type)
+        {
+            if (type.GetConstructor(Type.EmptyTypes) != null)
+                return Activator.CreateInstance(type);
+
+            // Type without parameterless constructor
+            return FormatterServices.GetUninitializedObject(type);
+        }
+
+    }
+}
